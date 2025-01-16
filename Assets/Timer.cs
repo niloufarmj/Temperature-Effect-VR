c9336cfd5def2f368b337363347f514a30917780
AhmedSh4hien <ahmedshahien4@gmail.com>
2025-01-16 18:53:34 +0100

    Add Hand Collider + Chain to Start and End

--- Assets/Timer.cs
+++ Assets/Timer.cs
@@ -2,26 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
-
 public class Timer : MonoBehaviour
 {
-    public float targetTime = 60f;
     public GameObject uiQuestions;
-    
-    void Update()
-    {
-        targetTime -= Time.deltaTime; 
-        
-        if (targetTime <= 0.0f)
-        {
-            timerEnded();
-        }       
-    }
 
     
 
-    void timerEnded()
+    public void timerEnded()
     {
         uiQuestions.SetActive(true);
         Debug.Log("TimerEnded");
