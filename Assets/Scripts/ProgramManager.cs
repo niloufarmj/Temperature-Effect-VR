868e3d1abb4ff0b51cf8eb0b1ed3515dc518d678
AhmedSh4hien <ahmedshahien4@gmail.com>
2025-01-27 15:55:46 +0100

    Nothing

--- Assets/Scripts/ProgramManager.cs
+++ Assets/Scripts/ProgramManager.cs
@@ -19,10 +19,31 @@ public class ProgramManager : MonoBehaviour
     private float phaseDuration = 10f;
     private float coolDownDuration = 40f;
 
+    public GameObject leftControllerPrefab, rightControllerPrefab;
+
     public GameObject uiLine;
 
     void Start()
     {
+        // OVRInput.SetControllerVibration(1, 1, OVRInput.Controller.LTouch);
+        // OVRInput.SetControllerVibration(1, 1, OVRInput.Controller.RTouch);
+        
+        // Vector3 leftControllerPosition = OVRInput.GetLocalControllerPosition(OVRInput.Controller.LTouch);
+        // Quaternion leftControllerRotation = OVRInput.GetLocalControllerRotation(OVRInput.Controller.LTouch);
+        // Vector3 rightControllerPosition = OVRInput.GetLocalControllerPosition(OVRInput.Controller.RTouch);
+        // Quaternion rightControllerRotation = OVRInput.GetLocalControllerRotation(OVRInput.Controller.RTouch);
+
+        // Debug.Log("Left Controller Position: " + leftControllerPosition);
+        // Debug.Log("Right Controller Position: " + rightControllerPosition);
+
+        // leftControllerPrefab.transform.position = leftControllerPosition;
+        // leftControllerPrefab.transform.rotation = leftControllerRotation;
+
+        // rightControllerPrefab.transform.position = rightControllerPosition;
+        // rightControllerPrefab.transform.rotation = rightControllerRotation;
+
+        // OVRInput.SetControllerVibration(0, 0, OVRInput.Controller.LTouch);
+        // OVRInput.SetControllerVibration(0, 0, OVRInput.Controller.RTouch);
         guideText.text = "Please put both hands inside the heaters.";
         
     }
